42e8d60ce7eda7e67a176b47d8442f9ba09f7158
Keksonadze <77896608+Keksonadze@users.noreply.github.com>
2021-01-24 11:36:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ async def on_ready():
     print("Bot is online")
  
 @bot.command(pass_content = True)
-async def kek():
+async def kek(ctx):
     await Bot.say("cock")
        
 token = os.environ.get('BOT_TOKEN')
