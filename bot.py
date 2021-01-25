3118978ffdddab21b9bcf4d031153c92e253cc65
Keksonadze <77896608+Keksonadze@users.noreply.github.com>
2021-01-25 22:14:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,4 +68,8 @@ async def dm(ctx):
     
 token = os.environ.get('BOT_TOKEN')
 
+@bot.command()
+async def spam(ctx, member: discorm_Member):
+    await member.send(f'ПРИВЕТ ОТ ОТБИВАЛЫ')
+
 bot.run(str(token))
